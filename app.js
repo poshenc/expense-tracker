8236f01eac8fe170920f4bdb644105af1ff05e81
johnson <poshenc@gmail.com>
2021-07-26 11:29:40 +0800

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-// 載入 express 並建構應用程式伺服器
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 
 // 設定首頁路由
 app.get('/', (req, res) => {
