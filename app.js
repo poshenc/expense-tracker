7dfa3f4b44b348ea5377a74391ea099713e78b3d
johnson <poshenc@gmail.com>
2021-07-26 17:46:58 +0800

    add detail page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const Record = require('./models/record')
+const bodyParser = require('body-parser')
+
 
 const app = express()
 
@@ -18,15 +20,40 @@ db.once('open', () => {
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
-// 設定首頁路由
+// 首頁路由
 app.get('/', (req, res) => {
   Record.find()
     .lean()
     .then(records => res.render('index', { records }))
     .catch(error => console.error(error))
 })
+
+//Record路由
+app.get('/records/new', (req, res) => {
+  return res.render('new')
+})
+
+//View Detail路由
+app.get('/records/:id', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .lean()
+    .then((record) => res.render('detail', { record }))
+    .catch(error => console.log(error))
+})
+
+
+//資料庫新增資料
+app.post('/records', (req, res) => {
+  const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
+  return Record.create({ name })     // 存入資料庫
+    .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
 
 // 設定 port 3000
 app.listen(3000, () => {
