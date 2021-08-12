3b9edc83c0e222f50f73a6ae24a929f230df31d1
johnson <poshenc@gmail.com>
2021-08-12 10:35:06 +0800

    add register page

--- routes/users.js
+++ routes/users.js
@@ -3,4 +3,15 @@ const router = express.Router()
 router.get('/login', (req, res) => {
   res.render('login')
 })
+
+router.post('/login', (req, res) => {
+
+})
+
+router.get('/register', (req, res) => {
+  res.render('register')
+})
+
+
+
 module.exports = router
