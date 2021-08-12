ce665fca6f8c2e7a9fe9557830c84645e1fdee9a
johnson <poshenc@gmail.com>
2021-08-13 00:15:46 +0800

    add facebook login and dotenv

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const { ifEqual } = require('./tools/handlebarshelpers')
 require('handlebars-helpers')()
 
@@ -12,13 +15,13 @@ const usePassport = require('./config/passport')
 require('./config/mongoose')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', helpers: { ifEqual }, extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(session({
-  secret: 'ThisIsMySecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const home = require('./modules/home')
 const records = require('./modules/records')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 const { authenticator } = require('../middleware/auth')
 
 
-router.use('/users', users)
 router.use('/records', authenticator, records)
+router.use('/users', users)
+router.use('/auth', auth)
 router.use('/', authenticator, home)
 
 module.exports = router
