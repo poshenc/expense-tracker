ca0c82f22dd819f9b0517a0186cb64c487770bc0
johnson <poshenc@gmail.com>
2021-08-16 03:30:16 +0800

    remove icon html text from database and place it in the views

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -8,23 +8,23 @@ const Category = require('../category')
 const categories = [
   {
     name: '家居物業',
-    icon: '<i class="fas fa-home fa-3x"></i>'
+    icon: 'fas fa-home fa-3x'
   },
   {
     name: '交通出行',
-    icon: '<i class="fas fa-shuttle-van fa-3x"></i>'
+    icon: 'fas fa-shuttle-van fa-3x'
   },
   {
     name: '休閒娛樂',
-    icon: '<i class="fas fa-grin-beam fa-3x"></i>'
+    icon: 'fas fa-grin-beam fa-3x'
   },
   {
     name: '餐飲食品',
-    icon: '<i class="fas fa-utensils fa-3x"></i>'
+    icon: 'fas fa-utensils fa-3x'
   },
   {
     name: '其他',
-    icon: '<i class="fas fa-pen fa-3x"></i>'
+    icon: 'fas fa-pen fa-3x'
   }
 ]
 
